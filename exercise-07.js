c31be5915f93285bafa2491626dd791eac292fa3
douglas7623 <douglasaquinocosta@gmail.com>
2022-06-21 11:32:40 -0300

    Add resolution exercise-07

---
+++ exercise-07.js
@@ -0,0 +1,7 @@
+const data = [ 1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3 ];
+
+data.splice(2, 0, 100)
+
+console.log('\n7. Inclua um elemento com o valor "100" na terceira posição.\n')
+
+console.log(data)
