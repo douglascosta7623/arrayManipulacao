cc910a7d15c125ecc9089822117b289ed1c61832
douglas7623 <douglasaquinocosta@gmail.com>
2022-06-21 10:50:18 -0300

    Add statement out us exercices 2 to 4

--- exercise-03.js
+++ exercise-03.js
@@ -1,3 +1,5 @@
 let data = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
 let maxValue = Math.max.apply(null, data);
+
+console.log("\n3. Encontre o maior valor. \n")
 console.log(maxValue);
--- exercise-04.js
+++ exercise-04.js
@@ -1,4 +1,5 @@
 const data = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
 
 const minValue = Math.min.apply(null, data);
+console.log("\n4. Encontre o menor valor. \n")
 console.log(minValue)
