fe656aaea6e0449ec2367efeaa0dd2ef719191c0
douglas7623 <douglasaquinocosta@gmail.com>
2022-06-21 10:48:18 -0300

    Add resolution Exercise-05

---
+++ exercise-05.js
@@ -0,0 +1,5 @@
+const data = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
+
+const result = data.splice(4, 1);
+console.log("Valor deletado: ", result)
+console.log("Nova array sem o elemento deletado: ", data)
