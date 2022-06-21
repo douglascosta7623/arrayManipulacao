1d9d77ee9ad520cc3f2adb0a24c1308fe3b2573b
douglas7623 <douglasaquinocosta@gmail.com>
2022-06-21 12:58:36 -0300

    Add resolution exercise-08

--- exercise-08.js
+++ exercise-08.js
@@ -2,9 +2,6 @@ const data = [ 1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3 ];
 
 const dataUnify = [...new Set(data)];
 
-
-
-
 console.log('\n8. Remova os valores duplicados.\n')
 
 console.log(dataUnify)
